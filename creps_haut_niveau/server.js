8a200fa81430b679d7d45be9f132d40e957ac6f3
Antoine Chalifour <antoine.chalifour@gmail.com>
2015-05-12 16:40:24 +0200

    Introduction + debut crédits

--- creps_haut_niveau/server.js
+++ creps_haut_niveau/server.js
@@ -2,10 +2,15 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 var dir = (env === 'dev') ? 'src' : 'build';
+var credits = require('./data/credits');
+
 // serve static content from the html directory
 app.use(express.static(path.join(__dirname, dir)));
+
+app.get('/api/credits/', function(req, res, next){
+  res.json(credits);
+});
 
 module.exports = app;
--- creps_haut_niveau/src/app/credits/credits.js
+++ creps_haut_niveau/src/app/credits/credits.js
@@ -2,10 +2,22 @@
   'use strict';
 
   angular.module('hyblab.creps')
+
+  .controller('CreditsCtrl', ['Credits', function(Credits){
+    var vm = this;
+
+    Credits.get()
+    .then(function(response){
+      vm.people = response.data;
+    });
+  }])
+
   .directive('crepsCredits', [function(){
     return {
       restrict: 'EA',
-      templateUrl: 'app/credits/credits.template.html'
+      templateUrl: 'app/credits/credits.template.html',
+      controller: 'CreditsCtrl',
+      controllerAs: 'credits'
     };
   }]);
 })();
