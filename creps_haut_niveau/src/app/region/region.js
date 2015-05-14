c774f9d0fbd9da66f06bd6fbecb56e1645491f11
Tom <tom.dupisre@gmail.com>
2015-05-15 01:26:24 +0200

    Dataviz1 bien avancée

--- creps_haut_niveau/src/app/region/region.js
+++ creps_haut_niveau/src/app/region/region.js
@@ -2,23 +2,37 @@
   'use strict';
 
   angular.module('hyblab.creps')
-  .controller('RegionCtrl', ['$q', 'Data',function($q, Data){
+  .controller('RegionCtrl', ['$scope', 'Data',function($scope, Data){
   	var vm = this;
+    vm.counthomme = 0;
+    vm.countfemme = 0;
 
     vm.region = Data.region;
     console.log(vm.region);
 
 
-    vm.hautNiveau = vm.region.hautNiveau.total;
-    vm.sportifsListes = vm.region.total;
-    vm.espoirs = vm.region.espoirs.total;
-    vm.partenaires = vm.region.partenaires;
+    vm.hautNiveau = 0;
+    vm.sportifsListes = 0;
+    vm.espoirs = 0;
+    vm.partenaires = 0;
 
     vm.moins18espoir = vm.region.espoirs.underage;
     vm.moins18hn = vm.region.hautNiveau.underage;
 
     vm.doughlabels = ["Mineurs de haut niveau","Majeurs de haut niveau"];
     vm.doughdata = [vm.moins18espoir,vm.moins18hn];
+
+    $scope.$on('slide:region',function(){
+      $scope.$apply(function(){
+        vm.counthomme = 63;
+        vm.countfemme = 37;
+
+        vm.hautNiveau = vm.region.hautNiveau.total;
+        vm.sportifsListes = vm.region.total;
+        vm.espoirs = vm.region.espoirs.total;
+        vm.partenaires = vm.region.partenaires;
+      });
+    });
            
   }])
   .directive('crepsRegion',[function(){
