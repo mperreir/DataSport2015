7731d676685277010188d4f6d897dabcb91963df
Julien Barret <ibarretj@gmail.com>
2015-05-15 16:06:29 +0000

    début lecture csv

--- ouest_france_running/client/app/main/main.controller.js
+++ ouest_france_running/client/app/main/main.controller.js
@@ -10,13 +10,15 @@ angular.module('hyblabApp')
   .controller('MainCtrl', function() {
     
     //Recupération des données
-    var csv = new CSV('../assets/data/25km.csv',
-    {
-      header : true,
-      cellDelimiter : ' '
+    var dataPath = "../../assets/data/25km.csv";
+    var csv = new Papa.parse(dataPath,{
+      download : true,
+      delimiter : '    ',
+      header: true,
+      encoding: 'UTF8',
+      skipEmptyLines: true,
+      complete: function(results) { console.log(results); }
     });
-    var csvParsed = csv.parse();
-    console.log(../assets/data/25km.csv);
 
     // Vide pour l'instant
 
